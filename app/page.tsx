1183e2808c3176ea13ec3bca714b752e02ae5efe
Metin Güler <senin-epostan@gmail.com>
2026-05-13 14:42:20 +0300

    fix: connecting local to github and updating files

--- app/page.tsx
+++ app/page.tsx
@@ -3,4 +3,5 @@ import { redirect } from "next/navigation";
 export default function Home() {
   // Kullanıcı ana sayfaya geldiğinde direkt giriş sayfasına gönderilir
   redirect("/login");
+  // geliyeo
 }
